357e8cba8ddc68d13c5d25c2fcf690b42c9c8475
davidokun <davidokun99@gmail.com>
2020-07-27 22:22:37 +0200

    Output total expenses amount

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const amount = document.querySelector('#amount');
 const btnClear = document.querySelector('#btn-clear');
 const btnAdd = document.querySelector('#btn-add');
 const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
+let totalExpenses = 0;
+
+const clear = () => {
+    reason.value = "";
+    amount.value = "";
+}
 
 btnAdd.addEventListener('click', () => {
     const enterReason = reason.value;
@@ -18,4 +26,11 @@ btnAdd.addEventListener('click', () => {
     newItem.textContent = enterReason + ": $" + enterAmount;
 
     expensesList.appendChild(newItem);
+
+    totalExpenses += +enterAmount;
+    totalExpensesOutput.textContent = totalExpenses;
+
+    clear();
 });
+
+btnClear.addEventListener('click', clear);
