964def3937b1abaac2e8033fd5518830f67ecbb8
davidokun <davidokun99@gmail.com>
2020-07-27 21:40:04 +0200

    Add js to buttons functionality

---
+++ app.js
@@ -0,0 +1,8 @@
+const reason = document.querySelector('#reason');
+const amount = document.querySelector('#amount');
+const btnClear = document.querySelector('#btn-clear');
+const btnAdd = document.querySelector('#btn-add');
+
+btnAdd.addEventListener('click', () => {
+    
+});
